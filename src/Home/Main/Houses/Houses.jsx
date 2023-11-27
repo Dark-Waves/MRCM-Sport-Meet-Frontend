9f9da30e5755c774e11c2a9bd1d21021573af95a
Dark Waves Tech <89140466+darkwaves-ofc@users.noreply.github.com>
2023-11-28 00:48:50 +0530

    Update Houses.jsx

--- src/Home/Main/Houses/Houses.jsx
+++ src/Home/Main/Houses/Houses.jsx
@@ -3,6 +3,9 @@ export default function Houses({ houseData }) {
   return (
     <div className="main__Houses">
       <h2 className="title font-xl font-weight-700">Houses</h2>
+      <div className="grid-houses">
+        .house_box
+      </div>
     </div>
   );
 }
